9840f32aa2a2530a6aeb79b6411df1de7c7b31b4
HRiveros <hugo.riveros05@inacapmail.cl>
2024-07-19 18:17:02 -0400

    agregar tuneado importancia plus Css tuneado Ft Width

--- mi-proyecto-nuevo/src/componentes/Notes.jsx
+++ mi-proyecto-nuevo/src/componentes/Notes.jsx
@@ -1,14 +1,23 @@
 export const Notes = ({nota}) => {
     const {id,titulo,descripcion,importante} = nota
     if (importante){
-    return (
-        <div>
-            <span className="close-btn">x</span>
-            <h2>{titulo}</h2>
-            <p>{descripcion}</p>
-        </div>
-    );
-};
+        return (
+            <div className="important">
+                <span className="close-btn">x</span>
+                <h2>{titulo}</h2>
+                <p>{descripcion}</p>
+            </div>
+        )
+    }
+        else {
+            return (
+                <div className="no-important">
+                    <span className="close-btn">x</span>
+                    <h2>{titulo}</h2>
+                    <p>{descripcion}</p>
+                </div>
+            )
+        }
 }
 
 export default Notes;
